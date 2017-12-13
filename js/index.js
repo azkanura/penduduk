0b25240c8e5e2ad2f13b40ef0e4e7f9c01327228
DESKTOP-H3EQUCC\USER <nurunalaazka@gmail.com>
2017-12-13 21:13:59 +0700

    Start trying to display data from firestore to web

--- js/index.js
+++ js/index.js
@@ -7,9 +7,12 @@
 // 		console.log(data);
 // 	});
 // });
-
+var container = $('.px-content');
 db.collection('penduduk').get().then((querySnapshot)=>{
 	querySnapshot.forEach((doc)=>{
-		console.log(`${doc.id}=>${doc.data().foto_orang}`);
+		if(doc && doc.exists){
+			data = doc.data();
+			container.append('Foto KTP: '+data.foto_ktp+'<br>Foto Orang: '+data.foto_orang+'<br>Jenis Kelamin: '+data.jenis_kelamin+'<br>Kecamatan: '+data.kecamatan+'<br>Kelurahan: '+data.kelurahan+'<br>Jenis kewarganegaraan: '+data.kewarganegaraan+'<br>Jenis Kisaran Penghasilan: '+data.kirasan_penghasilan+'<br>Kota: '+data.kota+'<br>Nama Lengkap:  '+data.nama_lengkap+' <br>Pekerjaan: '+data.pekerjaan+'<br>Pendidikan: '+data.pendidikan+'<br>Provinsi:  '+data.provinsi+'<br>RT:  '+data.rt+'<br>RW:  '+data.rw+'<br>Status Perkawinan:  '+data.status_perkawinan+'<br>Tempat Tanggal Lahir:  '+data.tempat_lahir+', '+data.tanggal_lahir+'<br><hr><br>');
+		}
 	});
 });
