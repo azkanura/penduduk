823dce61042a03d74fccfe606979c9138b2f3690
DESKTOP-H3EQUCC\USER <nurunalaazka@gmail.com>
2017-12-14 21:53:34 +0700

    implementing authentication

--- js/index.js
+++ js/index.js
@@ -17,3 +17,36 @@
 // 		}
 // 	});
 // });
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+
+    console.log(email);
+    $('display-user-name').html(email);
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+    $(location).attr('href','/login');
+  }
+});
+
+
+$('#logoutBtn').on('click',function(){
+	firebase.auth().signOut().catch((error)=>{
+		var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log('Error '+errorCode+': '+errorMessage);
+        alert('Error '+errorCode+': '+errorMessage);
+	});
+});
+
+
